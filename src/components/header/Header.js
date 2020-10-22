e9a63a2f45ed516591b0a35fac6b37c4e787dbfe
the-chornaya <me@chornaya.com>
2020-10-22 20:10:32 +0100

    add options to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import * as cn from './Header.module.css';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 export function Header(props) {
     const {numberOfFavMovies, searchMovies} = props;
     const [searchText, setSearchText] = React.useState('');
+    const location = useLocation();
+    const isFavouritePage = location.pathname === '/favourites';
 
     function onClickSearchMovies(event) {
         event.preventDefault();
@@ -15,6 +17,30 @@ export function Header(props) {
         setSearchText(event.target.value);
     }
 
+    const allMoviesLink = (
+        <Link to="/" className={cn.link}>
+            <div className={cn.movies}>
+                <div className={cn.headerIcon}>
+                    <i className="fas fa-film" />
+                </div>
+                <div className={cn.headerText}>All Movies</div>
+            </div>
+        </Link>
+    );
+
+    const favouriteLink = (
+        <Link to="/favourites" className={cn.link}>
+            <div className={cn.movies}>
+                <div className={cn.headerIcon}>
+                    <i className="fas fa-heart" />
+                </div>
+                <div className={cn.headerText}>
+                    Favourite Movies: <span>{numberOfFavMovies}</span>
+                </div>
+            </div>
+        </Link>
+    );
+
     return (
         <div className={cn.header}>
             <div className={cn.user}>
@@ -23,24 +49,7 @@ export function Header(props) {
                 </div>
                 <div className={cn.headerText}>User</div>
             </div>
-            <Link to="/favourites" className={cn.link}>
-                <div className={cn.movies}>
-                    <div className={cn.headerIcon}>
-                        <i className="fas fa-heart" />
-                    </div>
-                    <div className={cn.headerText}>
-                        Favourite Movies: <span>{numberOfFavMovies}</span>
-                    </div>
-                </div>
-            </Link>
-            <Link to="/" className={cn.link}>
-                <div className={cn.movies}>
-                    <div className={cn.headerIcon}>
-                        <i className="fas fa-film" />
-                    </div>
-                    <div className={cn.headerText}>All Movies</div>
-                </div>
-            </Link>
+            {isFavouritePage ? allMoviesLink : favouriteLink}
             <form onSubmit={onClickSearchMovies} className={cn.search}>
                 <input onChange={onChangeSearchMovies} className={cn.searchInput} type="text" />
                 <div onClick={onClickSearchMovies} className={cn.searchIcon}>
