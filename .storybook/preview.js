24ecd85366656edc739d0372db041248de30b635
the-chornaya <me@chornaya.com>
2020-10-27 21:27:55 +0000

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+export const parameters = {
+    actions: {argTypesRegex: '^on[A-Z].*'},
+    layout: 'fullscreen',
+    backgrounds: {
+        default: 'dark',
+    },
+    knobs: {
+        escapeHTML: false,
+    },
+};
